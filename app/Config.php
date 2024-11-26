2ac75f1c9bb01b9f6117fbd19881ecf6e19e8adb
Alejandro Vázquez Yerga <vazquez.yerga.alejandro@iescamas.es>
2024-11-26 14:25:40 +0100

    Cambios realizados en Config.php

--- app/Config.php
+++ app/Config.php
@@ -3,7 +3,7 @@
  class Config
  {
      static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentos72024";
+     static public $mvc_bd_nombre   = "alimento72024";
      static public $mvc_bd_usuario  = "mymangeldnd2";
      static public $mvc_bd_clave    = "ukOrevgI";
      static public $mvc_vis_css     = "estilo.css";
